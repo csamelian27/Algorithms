7195cb64ef897be70170b45f696b3b9ce6a1dc54
csamelian27 <csamelian@gmail.com>
2019-06-20 18:33:52 -0400

    added colt steele solution

--- search-DivideAndConquer.js
+++ search-DivideAndConquer.js
@@ -25,7 +25,7 @@ function search(arr, val) {
   return -1;
 }
 
-search([1,2,3,5,6,8,9,12,15,16,29], 15) 
+search([1,2,3,5,6,8,9,12,15,16,29], 15)
 
 
 // NAIVE APPROACH - start from left and loop through arr once to find the number or not
