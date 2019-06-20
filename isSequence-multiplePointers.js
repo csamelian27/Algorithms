2cd2b0c967ce8d0e2dc2161f4a4487694168405a
csamelian27 <csamelian@gmail.com>
2019-06-20 18:43:26 -0400

    working on isSubsequence

---
+++ isSequence-multiplePointers.js
@@ -0,0 +1,8 @@
+// Write a function called isSubsequence which takes in two strings and checks whether the characters in
+// the first string for a subsquence of characters in the second string.
+// In other words, the function should check whether the characters in the first string
+// appear somewhere in the second string, without their order changing.
+
+function isSubsequence(str1, str2) {
+  
+}
