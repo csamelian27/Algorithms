dbf5d8e44635c6fb6cdfbe4d28a356dcf16f30a5
csamelian27 <csamelian@gmail.com>
2019-07-02 17:59:23 -0400

    pure function collectStrings

--- collectStrings-helperMethodRecursion.js
+++ collectStrings-helperMethodRecursion.js
@@ -1,3 +1,6 @@
+// Write a function called collectStrings which accepts an object and returns an array of all the values in the object that have a typeof string.
+
+// Helper Method Recursion
 function collectStrings(obj) {
   let stringsArr = [];
    function gatherStrings(o) {
@@ -11,4 +14,18 @@ function collectStrings(obj) {
    }
    gatherStrings(obj);
    return stringsArr;
+}
+
+
+// Pure recursion
+function collectStrings(obj) {
+  let stringsArr = [];
+  for (let key in obj) {
+    if (typeof obj[key] === 'string') {
+      stringsArr.push(obj[key])
+    } else if(typeof obj[key] === 'object') {
+      stringsArr = stringsArr.concat(collectStrings(obj[key]));
+    }
+  }
+  return stringsArr;
 }
