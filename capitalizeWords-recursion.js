3957865812ec53568bcb9b9ae045f20575f8613b
csamelian27 <csamelian@gmail.com>
2019-07-02 18:00:53 -0400

    finishing instructions

--- capitalizeWords-recursion.js
+++ capitalizeWords-recursion.js
@@ -1,4 +1,5 @@
-
+// Write a recursive function called capitalizeWords.
+// Given an array of words, return a new array containing each word capitalized.
 
 function capitalizeWords(arr) {
   if(arr.length === 1) return [arr[0].toUpperCase()];
