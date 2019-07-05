f3f36f3eb452515744198c32ead158e112f1b824
csamelian27 <csamelian@gmail.com>
2019-07-05 17:01:58 -0400

    refactored

--- naiveStringsSearch.js
+++ naiveStringsSearch.js
@@ -14,15 +14,11 @@ function stringSearch(string, substr) {
 
   for (let i = 0; i < string.length; i++) {
     for (let j = 0; j < substr.length; j++) {
-      if (string[i+j] !== substr[j]) {
-        break;
-      }
-      if(j === substr.length -1) {
-        count++;
-      }
+      if (string[i+j] !== substr[j]) break;
+      if(j === substr.length -1) count++;
     }
   }
   return count;
 }
 
-stringSearch("Harold said haha in hamburg")
+stringSearch("Harold said haha in hamburg", "haha")
