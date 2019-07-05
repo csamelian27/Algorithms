5ae625a771132dbfd1475bc1e7f5e2e69f3b7292
csamelian27 <csamelian@gmail.com>
2019-07-05 16:52:11 -0400

    stringSearch setup

---
+++ naiveStringsSearch.js
@@ -0,0 +1,17 @@
+// Suppose you want to count the number of times a smaller string appears in a longer string
+// A straightforward approach involves checking pairs of characters individually
+
+// Define a function that accepts two strings, a larger string and a substr to search for
+// Loop over the longer string
+// Loop over the shorter string
+// If the characters don't match, break out of the inner loop
+// If the cahracters do match, keep going
+// If you complete the inner loop and find a match, increment the count of matches
+// Return the count
+
+function stringSearch(string, substr) {
+
+}
+
+
+"Harold said haha in hamburg"
