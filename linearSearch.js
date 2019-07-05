ac1d5b3ddad815a2a1ff3f2e9de9985b70cf712c
csamelian27 <csamelian@gmail.com>
2019-07-05 15:55:52 -0400

    my solution

--- linearSearch.js
+++ linearSearch.js
@@ -2,3 +2,12 @@
 // Loop through the array and check if the current array element is equal to the value
 // If it is, return the index at which the element is found
 // If the value is never found, return -1
+
+function linearSearch(arr, value) {
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] === value) {
+      return i;
+    }
+  }
+  return -1;
+}
