37e6d733aea183cb28bb27969410fbcc9212981c
csamelian27 <csamelian@gmail.com>
2019-07-07 17:50:24 -0400

    insertionSort setup

---
+++ insertionSort.js
@@ -0,0 +1,14 @@
+// Define a function called insertionSort that takes in an array
+// Start by picking the second element in the array.
+// Now compare the second element with the one before it and swap if necessary.
+// Continue to the next element and if it is in the incorrect order, iterate through the sorted portion (i.e. to the left side) to place the element in the correct place.
+// Repeate until the array is sorted.
+
+function insertionSort(arr) {
+  for (let i = 0; i < arr.length; i++) {
+    for (let i = 0; i < arr.length; i++) {
+
+    }
+  }
+  return arr;
+}
