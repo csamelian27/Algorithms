ae4106dfc57f92d9125d5b5f67f5fcc4d1a32ab6
csamelian27 <csamelian@gmail.com>
2019-07-07 18:00:00 -0400

    solved insertionSort

--- insertionSort.js
+++ insertionSort.js
@@ -5,10 +5,12 @@
 // Repeate until the array is sorted.
 
 function insertionSort(arr) {
-  for (let i = 0; i < arr.length; i++) {
-    for (let i = 0; i < arr.length; i++) {
-
+  for (let i = 1; i < arr.length; i++) {
+    let current = arr[i];
+    for (let j = i-1; j >= 0 && arr[j] > current; j--) {
+      arr[j+1] = arr[j];
     }
+    arr[j+1] = current;
   }
   return arr;
 }
