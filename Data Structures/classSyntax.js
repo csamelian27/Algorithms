72ceb0497d6c5f793f8cafb2698c94194a7ecb84
csamelian27 <csamelian@gmail.com>
2019-07-10 16:43:01 -0400

    class setup with instantiations

--- Data Structures/classSyntax.js
+++ Data Structures/classSyntax.js
@@ -1,7 +1,11 @@
-// 
+// The method to create new objects must be called constructor
+// The class keyword creates a constant, so you can not redefine it.
 class Student {
   constructor(firstName, lastName) {
     this.firstName = firstName;
     this.lastName = lastName;
   }
 }
+
+let firstStudent = new Student("Colt", "Steele");
+let secondStudent = new Student("Blue", "Steele");
