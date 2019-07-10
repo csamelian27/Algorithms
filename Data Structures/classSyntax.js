4641917ca9e3ee9045521c0ed090b6b43cfac928
csamelian27 <csamelian@gmail.com>
2019-07-10 16:46:33 -0400

    trying to push

--- Data Structures/classSyntax.js
+++ Data Structures/classSyntax.js
@@ -1,11 +1,16 @@
 // The method to create new objects must be called constructor
 // The class keyword creates a constant, so you can not redefine it.
 class Student {
-  constructor(firstName, lastName) {
+  constructor(firstName, lastName, year) {
     this.firstName = firstName;
     this.lastName = lastName;
+    // naming doesn't matter, just matters what we set the value to
+    this.grade = year;
   }
 }
 
-let firstStudent = new Student("Colt", "Steele");
+let firstStudent = new Student("Colt", "Steele", 3);
+// grade is undefined if not given a param
 let secondStudent = new Student("Blue", "Steele");
+
+console.log(firstStudent.firstName);
