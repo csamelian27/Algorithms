62e03a69bed383f93a613b9cfbab946b22dd40a4
csamelian27 <csamelian@gmail.com>
2019-07-11 11:23:32 -0400

    setting up class structure for singly linked list

--- Data Structures/singlyLinkedList.js
+++ Data Structures/singlyLinkedList.js
@@ -1,5 +1,28 @@
-class LinkedList {
-  constructor() {
-    
+// piece of data - val
+// reference to next node - next
+
+class Node {
+  constructor(val) {
+    this.val = val;
+    this.next = null;
   }
 }
+
+// basic view of a linked list
+// let first = new Node("Hi")
+// first.next = new Node("there")
+// first.next.next = new Node("how")
+// first.next.next.next = new Node("are")
+// first.next.next.next.next = new Node("you?")
+
+
+class SinglyLinkedList {
+  constructor() {
+
+  }
+}
+
+// don't initialize a linked list with any data
+let list = new SinglyLinkedList()
+list.push("HELLO")
+list.push("GOODBYE")
