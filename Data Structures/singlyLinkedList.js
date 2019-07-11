34177547d204fdb7d29459ff3b8379fd2e87a051
csamelian27 <csamelian@gmail.com>
2019-07-11 11:29:50 -0400

    my implementation of push method

--- Data Structures/singlyLinkedList.js
+++ Data Structures/singlyLinkedList.js
@@ -16,10 +16,32 @@ class Node {
 // first.next.next.next.next = new Node("you?")
 
 
+// Push method psuedocode:
+// This function should accept a value.
+// Create a new node using the value passed to the function
+// If there is no head property on the list, set the head and tail to be the newly created node
+// Otherwise set the next property on the tail to be the new node and set the tail property on the list to be the newly created node
+// Increment the length by one
+
+
 class SinglyLinkedList {
   constructor() {
-
+    this.head = null;
+    this.tail = null;
+    this.length = 0;
   }
+
+  push(val) {
+    if(this.length = 0) {
+      this.head = val;
+      this.tail = val;
+    } else {
+      this.tail.next = val;
+      this.tail = val;
+    }
+    this.length += 1;
+  }
+
 }
 
 // don't initialize a linked list with any data
