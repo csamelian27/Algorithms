e35a19e65d147a478cf6a9d40d9734db83715883
csamelian27 <csamelian@gmail.com>
2019-07-11 12:15:42 -0400

    my unshift implementation

--- Data Structures/singlyLinkedList.js
+++ Data Structures/singlyLinkedList.js
@@ -46,6 +46,16 @@ first.next.next.next.next = new Node("you?")
 // Decrement the length by 1
 // Return current head/shifted off head node value
 
+
+// Unshift: (adds a node to the beginning of the list)
+// This function should accept a value
+// Create a new node using the value passed to the function
+// If there is no head property on the list, set the head and tail to be the newly created node
+// Otherwise set the newly created node's next property to be the current head property on the list
+// Set the head property on the list to be that newly created node
+// Increment the length of the list by 1
+// Return the linked list
+
 class SinglyLinkedList {
   constructor() {
     this.head = null;
@@ -103,6 +113,20 @@ class SinglyLinkedList {
       this.tail = null;
     }
     return currentHead;
+  }
+
+  unshift(val) {
+    let newNode = new Node(val);
+    if(!this.head) {
+      this.head = newNode;
+      this.tail = this.head;
+    } else {
+      let oldHead = this.head;
+      this.head = newNode;
+      this.head.next = oldHead;
+    }
+    this.length++;
+    return this;
   }
 }
 
