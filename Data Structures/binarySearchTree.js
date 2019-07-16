99506bc20b924fd098eaef6a6c6bec28de952fe7
csamelian27 <csamelian@gmail.com>
2019-07-16 15:57:52 -0400

    BST insert implementation

--- Data Structures/binarySearchTree.js
+++ Data Structures/binarySearchTree.js
@@ -11,6 +11,32 @@ class BinarySearchTree {
   constructor() {
     this.root = null;
   }
+
+  insert(val) {
+    let newNode = new Node(val);
+    if(!this.root) {
+      this.root = newNode;
+      return this;
+    } else {
+      let current = this.root;
+      while (true) {
+        if(val === current.val) return undefined;
+        if (val < current.val) {
+          if (!current.left) {
+            current.left = newNode;
+            return this;
+          }
+          current = current.left;
+        } else if(val > current.val) {
+          if(!current.right) {
+            current.right = newNode;
+            return this;
+          }
+          current = current.right;
+        }
+      }
+    }
+  }
 }
 
 // Inserting Pseudocode:
