b690fa39fe62eceab7162c2fc93240c359fdc1a0
csamelian27 <csamelian@gmail.com>
2019-07-18 10:19:09 -0400

    hash table done

--- Data Structures/hashTable.js
+++ Data Structures/hashTable.js
@@ -34,6 +34,34 @@ class HashTable {
     return undefined;
   }
 
+  keys() {
+    let keysArr = [];
+    for (let i = 0; i < this.keyMap.length; i++) {
+      if (this.keyMap[i]) {
+        for (let j = 0; j < this.keyMap[i].length; j++) {
+          if (!keysArr.includes(this.keyMap[i][j][0])) {
+            keysArr.push(this.keyMap[i][j][0]);
+          }
+        }
+      }
+    }
+    return keysArr;
+  }
+
+  values() {
+    let valuesArr = [];
+    for (let i = 0; i < this.keyMap.length; i++) {
+      if (this.keyMap[i]) {
+        for (let j = 0; j < this.keyMap[i].length; j++) {
+          if (!valuesArr.includes(this.keyMap[i][j][1])) {
+            valuesArr.push(this.keyMap[i][j][1]);
+          }
+        }
+      }
+    }
+    return valuesArr;
+  }
+
 }
 
 // Set / Get Pseudocode:
@@ -45,3 +73,9 @@ class HashTable {
     // 1. Accepts a key
     // 2. Hashes the key
     // 3. Retrieves the key-value pair
+
+// Keys / Values Pseudocode:
+  // Keys:
+    // 1. Loops through the hash table array and returns an array of keys in the table
+  // Values:
+    // 1. Loops through the hash table array and returns an array of values in the table
